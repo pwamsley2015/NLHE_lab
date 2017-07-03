61b08b6a1455a794bd25868fe6f538794a0695f7
Patrick Wamsley <pwamsley2015@francisparker.org>
2017-07-03 18:17:30 -0400

    added is<HandType> methods

--- src/game_components/HandEvaluator.java
+++ src/game_components/HandEvaluator.java
@@ -1,5 +1,12 @@
 package game_components;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+
+import game_components.Card.Suit;
+import static game_components.Card.*; 
+
 /**
  * Statically used to evaulate {@code Unmade Hands} into {@code MadeHands}
  * @author Patrick Wamsley
@@ -24,4 +31,186 @@ public class HandEvaluator {
 		return null; 
 	}
 	
+	private HashMap<Integer, Integer> getRankHistogram(UnmadeHand hand) {
+		
+		HashMap<Integer, Integer> histogram = new HashMap<>(); 
+		
+		for (Card c : hand.cards) {
+			if (histogram.get(c.rank) == null) {
+				histogram.put(c.rank, 1); 
+			} else {
+				histogram.put(c.rank, histogram.get(c.rank) + 1); 
+			}
+		}
+		return histogram; 
+	}
+	
+	private HashMap<Suit, Integer> getSuitHistogram(UnmadeHand hand) {
+		
+		HashMap<Suit, Integer> histogram = new HashMap<>(); 
+		
+		for (Card c : hand.cards) {
+			if (histogram.get(c.suit) == null) {
+				histogram.put(c.suit, 1); 
+			} else {
+				histogram.put(c.suit, histogram.get(c.suit) + 1); 
+			}
+		}
+		
+		return histogram; 
+	}
+	
+	
+	private boolean isStraightFlush(UnmadeHand hand, HashMap<Suit, Integer> histrogram) {
+		
+		if (isStraightFlushWheel(hand, histrogram)) {
+			return true; 
+		}
+		
+		Arrays.sort(hand.cards, new Comparator<Card>() {
+
+			@Override
+			public int compare(Card o1, Card o2) {
+				int deltaSuit = o1.compareTo(o2); //don't care how this is ordered
+				return deltaSuit != 0 ? deltaSuit : o1.rank - o2.rank; 
+			}
+		});
+		
+		int amountToStraightFlush = 1;
+		Card previous = BLUE_EYES_WHITE_DRAGON; 
+		
+		for (Card c : hand.cards) {
+			if (c.rank - 1 == previous.rank && c.suit == previous.suit) {
+				amountToStraightFlush++;
+				if (amountToStraightFlush == 5) {
+					return true; 
+				} 
+			} else {
+				amountToStraightFlush = 1; 
+			}
+		}
+		
+		return false; 
+	}
+	
+	private boolean isQuads(HashMap<Integer, Integer> histogram) {
+		for (int count : histogram.values()) 
+			if (count == 4) 
+				return true;
+		
+		return false;  
+	}
+	
+	private boolean isFullHouse(HashMap<Integer, Integer> histogram) {
+		return isTrips(histogram) && isTwoPair(histogram); 
+	}
+	
+	private boolean isFlush(HashMap<Suit, Integer> histogram) {
+		for (int count : histogram.values()) 
+			if (count >= 5) 
+				return true;
+		
+		return false; 
+	}
+	
+	private boolean isStraight(UnmadeHand hand) {
+		
+		if (isWheel(hand)) {
+			return true; 
+		}
+		
+		Arrays.sort(hand.cards); 
+		
+		
+		int amountToStraight = 1; 
+		int previousRank = Integer.MIN_VALUE; 
+		for (Card c : hand.cards) {
+			if (c.rank - 1 == previousRank) {
+				amountToStraight++; 
+				if (amountToStraight == 5) {
+					return true; 
+				}
+			} else {
+				amountToStraight = 1; 
+			}
+			previousRank = c.rank; 
+		}
+		
+		return false; 
+	}
+	
+	private boolean isTrips(HashMap<Integer, Integer> histogram) {
+		for (int count : histogram.values()) 
+			if (count == 3) 
+				return true;
+		
+		return false; 
+	}
+	
+	private boolean isTwoPair(HashMap<Integer, Integer> histogram) {
+		int numPairs = 0; 
+		
+		for (int count : histogram.values()) 
+			if (count >= 2) 
+				numPairs++;
+		
+		return numPairs >= 2;  
+	}
+	
+	private boolean isPair(HashMap<Integer, Integer> histogram) {
+		
+		for (int count : histogram.values()) 
+			if (count == 2) 
+				return true;
+		
+		return false; 
+	}
+	
+	private boolean isStraightFlushWheel(UnmadeHand hand, HashMap<Suit, Integer> histogram) {
+		
+		Suit flushedSuit = null; 
+		
+		for (Suit s : histogram.keySet()) 
+			if (histogram.get(s) >= 5)
+				flushedSuit = s; 
+		
+		if (flushedSuit == null)
+			return false; 
+		
+		boolean ace = false, two = false, three = false, four = false, five = false; 
+		
+		for (Card c : hand.cards) {
+			if (c.rank == ACE && c.suit == flushedSuit)
+				ace = true; 
+			if (c.rank == TWO && c.suit == flushedSuit)
+				two = true; 
+			if (c.rank == THREE && c.suit == flushedSuit)
+				three = true; 
+			if (c.rank == FOUR && c.suit == flushedSuit)
+				four = true; 
+			if (c.rank == FIVE && c.suit == flushedSuit)
+				five = true; 
+		}
+		
+		return ace && two && three && four && five; 
+	}
+	
+	private boolean isWheel(UnmadeHand hand) {
+		boolean ace = false, two = false, three = false, four = false, five = false;  
+		
+		for (Card c : hand.cards) {
+			if (c.rank == ACE)
+				ace = true; 
+			if (c.rank == TWO)
+				two = true; 
+			if (c.rank == THREE)
+				three = true; 
+			if (c.rank == FOUR)
+				four = true; 
+			if (c.rank == FIVE)
+				five = true; 
+		}
+		
+		return ace && two && three && four && five;
+	}
 }
